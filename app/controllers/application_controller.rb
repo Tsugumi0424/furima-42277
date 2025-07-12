503ff22cbf44654ac3c9bfc577a74125a4ec9b4b
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-12 17:50:24 +0900

    ストラングパラメーターの指示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
 
@@ -13,6 +14,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
+  def configure_permitted_parameters
+    added_attrs = [:nickname, :last_name, :first_name,:last_name_kana, :first_name_kana, :birth_date]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+  end
 
 end
