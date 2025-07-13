63c8395a71f89c4b945ba128e7b3ac956205fe8b
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-14 00:27:18 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,17 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :shipping_cost, :prefecture, :shipping_time
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_time
+
+  VALID_PRICE_REGEX = /\A[0-9]+\z/
+
+  validates :title, presence: { message: "can't be blank"}
+  validates :description, presence: { message: "can't be blank"}
+  validates :price, presence: true, format: { with: VALID_PRICE_REGEX,  message: "is not a number"}
+  validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_time_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
 
 end
