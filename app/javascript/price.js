05eea058bc8a177a6a1d14007f0016b86a8b7699
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-16 02:07:56 +0900

    商品価格の表示機能

---
+++ app/javascript/price.js
@@ -0,0 +1,21 @@
+window.addEventListener("turbo:load", () => {
+
+  const priceInput = document.getElementById("item-price");
+  if (!priceInput) return;
+
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profitPrice = document.getElementById("profit");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = Number(priceInput.value);
+    if (!isNaN(inputValue)) {
+      addTaxPrice.innerHTML = Math.floor(inputValue * 0.1);
+      profitPrice.innerHTML = Math.floor(inputValue - (inputValue * 0.1));
+    }
+    else {
+      addTaxPrice.innerHTML = "";
+      profitPrice.innerHTML = "";
+    }
+
+  })
+});
