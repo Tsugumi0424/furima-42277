95710493f00c62ed48170f694cfddd95e26a13af
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-17 22:04:06 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
 
   root "items#index"
 
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show, :edit]
 
 end
