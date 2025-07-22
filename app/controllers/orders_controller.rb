418d344c62079ab48e40c555a85bb5b5b0e8c454
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-22 22:45:39 +0900

    orderコントローラーの定義
    環境変数設定後

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,45 @@
 class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order = OrderForm.new
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order = OrderForm.new(order_params)
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_form).permit(
+      :post_code,
+      :prefecture_id,
+      :municipality,
+      :street_number,
+      :building,
+      :phone_number,
+      :user_id,
+      :item_id
+    ).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+
 end
