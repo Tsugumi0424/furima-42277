c1b200e49aca904c06aaac587ab76fa7f4aedd8d
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-22 22:46:53 +0900

    addressコントローラー、モデルの修正
    不要だったためアクション削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :order
-
-  validates :post_code, presence: true
-  validates :prefecture_id,
-            numericality: { other_than: 0, message: "can't be blank" }
-  validates :municipality, presence: true
-  validates :street_number, presence: true
-  validates :phone_number, presence: true
-                            numericality: { only_integer: true}
-
 end
