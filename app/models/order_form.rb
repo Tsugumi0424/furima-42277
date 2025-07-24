a3a6ba3d9c23c511daeeb0f3ed7be350c80636e0
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-25 02:03:36 +0900

    order_formモデルのバリデーション修正、メソッドの修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -16,12 +16,10 @@ class OrderForm
     validates :phone_number, presence: true,
               length: { minimum: 10, maximum: 11 , message: "is too short" },
               numericality: { only_integer: true, message: "is invalid. Input only number" }
-    validates :user_id, presence: true
-    validates :item_id, presence: true
 
     def save
       order = Order.create(user_id: user_id, item_id: item_id)
-      Address.create(
+      Address.create!(
         post_code: post_code,
         prefecture_id: prefecture_id,
         municipality: municipality,
