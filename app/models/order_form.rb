e71b4e873dfaa2a52358d0fbcc59f78092e8812b
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-25 00:28:49 +0900

    order_formモデルのバリデーション追加

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -16,7 +16,8 @@ class OrderForm
     validates :phone_number, presence: true,
               length: { minimum: 10, maximum: 11 , message: "is too short" },
               numericality: { only_integer: true, message: "is invalid. Input only number" }
-    
+    validates :user_id, presence: true
+    validates :item_id, presence: true
 
     def save
       order = Order.create(user_id: user_id, item_id: item_id)
