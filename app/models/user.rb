bdc841c04069d1630aee6b817035bf10da0faf83
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-25 20:40:01 +0900

    userモデルアソシエーション追加、バリデーションの書き方修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :orders
 
   VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   VALID_KANA_REGEX = /\A[ァ-ヶー]+\z/
   VALID_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/
 
-  validates :nickname, presence: true
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' },
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' },
                        if: :password_required?
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters' }
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters' }
-  validates :first_name_kana, presence: true,
-                              format: { with: VALID_KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
-  validates :last_name_kana, presence: true,
-                             format: { with: VALID_KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
-  validates :birth_date, presence: true
+    validates :last_name, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters' }
+    validates :first_name, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters' }
+    validates :last_name_kana,format: { with: VALID_KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
+    validates :first_name_kana,format: { with: VALID_KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
+  end
 end
